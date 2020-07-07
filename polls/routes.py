a48694dce22670c0777b6784202f3d1b514ed88e
Timofey Danshin <t_danshin@gcore.lu>
2020-07-07 16:52:11 +0300

    Add login form and getting user from db

--- polls/routes.py
+++ polls/routes.py
@@ -1,5 +1,7 @@
-from polls.views import index
+from polls.views import index, login_post, login_get
 
 
 def setup_routes(app):
     app.router.add_get('/', index)
+    app.router.add_get('/login', login_get)
+    app.router.add_post('/login', login_post)
--- polls/views.py
+++ polls/views.py
@@ -1,4 +1,10 @@
+import hashlib
+import logging
+
 import aiohttp_jinja2
+from aiohttp import web
+from aiohttp_security import remember
+from sqlalchemy import text
 
 from . import db
 
@@ -11,3 +17,25 @@ async def index(request):
         questions = [dict(q) for q in records]
 
     return {'questions': questions}
+
+
+async def login_post(request):
+    async with request.app['db'].acquire() as conn:
+        data = await request.post()
+        login = data['login']
+        password = hashlib.sha3_256(data['password'].encode()).hexdigest()
+        statement = text(
+            """SELECT * from users where login=:login and password=:password;""")
+        cursor = await conn.execute(statement, login=login, password=password)
+        records = [dict(u) for u in await cursor.fetchall()]
+        logging.error(f'user: {records}')
+        # redirect_response = web.HTTPFound('/')
+        # await remember(request, redirect_response, 'jack')
+        # raise redirect_response
+
+    return web.Response(text=str(records))
+
+
+@aiohttp_jinja2.template('login.html')
+async def login_get(request):
+    return {}
